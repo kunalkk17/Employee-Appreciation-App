c217de43bbd31917076f00605a2ca367e9393696
Kunal Kumar <kunalkumar.0017@gmail.com>
2020-04-06 04:55:00 +0530

    model changed

--- Models/User.js
+++ Models/User.js
@@ -6,6 +6,13 @@ const UserSchema = new Schema({
     userName: {type:String, required:true},
     employeeId:{type:Number,required:true},
     password: {type:String, required:true},
+    coinBalance:{type:Number, default:100},
+    recievedCoins:{type:Number, required:false},
+    coinHistory:[{
+                   noOfCoins:{type:Number,default:null},
+                   senderId:{type:Number,default:null},
+                   reason:{type:String,default:null}
+                }],
     date: {type:Date, default:Date.now}
 })
 
